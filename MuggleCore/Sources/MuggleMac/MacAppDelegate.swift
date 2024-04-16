eb9024e476f511bd07332e443d384f7b328508eb
Ryan Ferrell <importRyan@gmail.com>
2024-04-15 21:52:30 -0700

    feat(LaunchAtLoginClient): extract, refactor state for exclusivity

--- MuggleCore/Sources/MuggleMac/MacAppDelegate.swift
+++ MuggleCore/Sources/MuggleMac/MacAppDelegate.swift
@@ -7,6 +7,7 @@ public final class MacAppDelegate: NSObject, ObservableObject {
   package lazy private(set) var central = BluetoothCentral(
     knownPeripheralsStore: .live(store: localStorage)
   )
+  @MainActor lazy var launchAtLogin = LaunchAtLoginClient()
   let localStorage: KeyValueStore = UserDefaults.standard
 }
 
@@ -15,6 +16,7 @@ extension MacAppDelegate: NSApplicationDelegate {
     localStorage[.launches, default: 0] &+= 1
     Log.app.info("Launch #\(self.localStorage[.launches, default: 0])")
     central.setup()
+    launchAtLogin.refresh()
   }
 
   public func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
--- MuggleCore/Sources/MuggleMac/Settings/Components/LaunchAtLoginToggle.swift
+++ MuggleCore/Sources/MuggleMac/Settings/Components/LaunchAtLoginToggle.swift
@@ -1,98 +1,32 @@
 #if canImport(ServiceManagement)
-import Common
-import Combine
-import ServiceManagement
 import SwiftUI
 
 struct LaunchAtLoginToggle: View {
-  @StateObject private var launchAtLogin = LaunchAtLoginViewModel()
+  @EnvironmentObject var delegate: MacAppDelegate
 
   var body: some View {
-    HStack {
-      Toggle(
-        "Start on boot",
-        isOn: .init(
-          get: { launchAtLogin.isLaunchingAtLogin },
-          set: { _ in launchAtLogin.toggle() }
+    Contents(client: delegate.launchAtLogin)
+  }
+
+  struct Contents: View {
+    @ObservedObject var client: LaunchAtLoginClient
+
+    var body: some View {
+      HStack {
+        Toggle(
+          "Start on boot",
+          isOn: .init(
+            get: { client.state.isLaunching },
+            set: { _ in client.toggle() }
+          )
         )
-      )
-      if launchAtLogin.requiresSystemPreferencesAction {
-        Button("Open Preferences") { launchAtLogin.openPreferences() }
-          .buttonBorderShape(.capsule)
-          .buttonStyle(.borderedProminent)
-          .controlSize(.small)
+        if client.state.requiresUserIntervention {
+          Button("Open Preferences", action: client.openPreferences)
+            .buttonBorderShape(.capsule)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.small)
+        }
       }
-    }
-    .onAppear(perform: launchAtLogin.refresh)
-  }
-}
-
-@MainActor
-final class LaunchAtLoginViewModel: ObservableObject {
-  @Published var isLaunchingAtLogin = false
-  @Published var requiresSystemPreferencesAction = false
-}
-
-extension LaunchAtLoginViewModel {
-  func toggle() {
-    refresh()
-
-    if isLaunchingAtLogin {
-      Task { @MainActor in
-        await unregister()
-        refresh()
-      }
-      return
-    }
-
-    if requiresSystemPreferencesAction {
-      SMAppService.openSystemSettingsLoginItems()
-      return
-    }
-
-    register()
-  }
-
-  func refresh() {
-    Log.app.info("SMAppService \(SMAppService.mainApp.status.rawValue)")
-    switch SMAppService.mainApp.status {
-    case .enabled:
-      isLaunchingAtLogin = true
-    case .notFound:
-      isLaunchingAtLogin = false
-      Log.app.error("SMSAppService notFound")
-    case .notRegistered:
-      isLaunchingAtLogin = false
-    case .requiresApproval:
-      isLaunchingAtLogin = false
-      requiresSystemPreferencesAction = true
-    @unknown default:
-      Log.app.error("SMSAppService unknown status")
-    }
-  }
-
-  func openPreferences() {
-    SMAppService.openSystemSettingsLoginItems()
-  }
-}
-
-private extension LaunchAtLoginViewModel {
-  func register() {
-    do {
-      try SMAppService.mainApp.register()
-      isLaunchingAtLogin = true
-    } catch {
-      Log.app.error("SMAppService register \(error.localizedDescription)")
-    }
-  }
-
-  func unregister() async {
-    do {
-      try await SMAppService.mainApp.unregister()
-      isLaunchingAtLogin = false
-    } catch {
-      isLaunchingAtLogin = false
-      Log.app.error("SMAppService unregister \(error.localizedDescription)")
     }
   }
 }
