2616dcbf9208fcb8c59fac953bbc44f5565a6c39
Ryan Ferrell <importRyan@gmail.com>
2024-04-22 12:19:11 -0700

    fix: find last tag for TestFlight commit list

--- ci_scripts/ci_post_xcodebuild.sh
+++ ci_scripts/ci_post_xcodebuild.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
-
-CURRENT_TAG=$(git describe --tags --abbrev=0 --match 'v*')
-LAST_TAG=$(git describe --tags --abbrev=0 --match 'v*' --contains $CURRENT_TAG | grep -v $CURRENT_TAG | tail -n 1)
+#!/bin/zsh
 
 if [[ -d "$CI_APP_STORE_SIGNED_APP_PATH" ]]; then
-    git fetch --deepen 1
-    git log $LAST_TAG..$CURRENT_TAG --pretty=format:"%s" >> ../TestFlight/WhatToTest.en-US.txt
+  git fetch --deepen 1
+
+  CURRENT_TAG=$(git describe --tags --abbrev=0 --match 'v*')
+  LAST_TAG=$(git describe --tags --abbrev=0 --match 'v*' $(git rev-list --tags --max-count=1 --skip=1 --no-walk) 2>/dev/null)
+
+  git log --pretty=format:"%s" $LAST_TAG..$CURRENT_TAG >> WhatToTest.en-US.txt
 fi
