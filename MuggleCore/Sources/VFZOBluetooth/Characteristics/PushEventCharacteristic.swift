1e837d16e0b72d791e40111f09b84d15b786d6aa
Ryan Ferrell <importRyan@gmail.com>
2024-04-26 14:18:23 -0700

    feat(VFZO): CB8 characteristic response decoding

--- MuggleCore/Sources/VFZOBluetooth/Characteristics/PushEventCharacteristic.swift
+++ MuggleCore/Sources/VFZOBluetooth/Characteristics/PushEventCharacteristic.swift
@@ -2,35 +2,98 @@ import Common
 import Foundation
 
 /// FFF0: 0783b03e8535b5a07140a304d2495cb8
+/// Notifying layout
+/// 0-1: Always [85, 170]
+/// 2: isHeating
+/// 3: isFahrenheit
+/// 4: literal current temperature
+/// 5: literal target temperature
+/// 6: isCharging
+/// 7: literal battery bars shown in app
+/// 8-17: Always 0
+/// 18: Always UInt8 16
+/// 19: Checksum (2-7 + zeros + 18)
+///
+/// Some rare responses have radically different payloads (either sniffer noise or different message)
+///
+/// When reading, responds with [67, 72, 65, 82, 49, 95, 86, 65, 76, 85, 69] "CHAR1_VALUE"
 final class PushEventCharacteristic: BluetoothCharacteristic {
   @Published var value: Contents?
   var characteristic: CBCharacteristic?
 
+  static let validTargetTemperatureRange = 35.0...65.0
+
   struct Contents: CustomDebugStringConvertible {
-    let isCharging: Bool
-    let celsius: Double
+    let battery: BatteryState
+    let isHeating: Bool
+    let tempCurrent: TemperatureState
+    let tempTarget: HeaterState
 
     var debugDescription: String {
-      "\(Self.self) isCharging: \(isCharging) celsius: \(Int(celsius))"
+      let unit = tempCurrent.unit == .fahrenheit ? "F" : "C"
+      return "\(Self.self) battery: \(battery.debugDescription) current: \(tempCurrent.formatted)\(unit) target: \(tempTarget.debugDescription)"
     }
   }
 
   func parse(update data: Data) throws {
-    guard data.bytes.endIndex == 20 else {
+    // Possibly just sniffer error, but some rare payloads differ and may represent a different message
+    guard data.bytes.endIndex == 20,
+          data.bytes.prefix(2) == [85, 170],
+          data.bytes[2...18].reduce(0, +) == data.bytes[19]
+    else {
       throw BluetoothParsingError.unexpectedValue(data: data)
     }
 
-    let numericalBoolean: Int8 = try data.readInteger(from: 6)
-    guard [0, 1].contains(numericalBoolean) else {
-      throw BluetoothParsingError.unexpectedValue("\(Self.self) \(numericalBoolean)")
+    let isHeating = data.bytes[2] == 1
+    guard [0, 1].contains(data.bytes[2]) else {
+      throw BluetoothParsingError.unexpectedValue("\(Self.self) isHeating \(isHeating)")
     }
 
-    // TODO: - Get some ice and plot. Change to F setting; it appears they transmit in F or C.
-    let rawTemperature: UInt8 = data.last!
+    let isFahrenheit = data.bytes[3] == 1
+    let unit = isFahrenheit ? UnitTemperature.fahrenheit : .celsius
+    guard [0, 1].contains(data.bytes[3]) else {
+      throw BluetoothParsingError.unexpectedValue("\(Self.self) isFahrenheit \(isFahrenheit)")
+    }
 
-    self.value = .init(
-      isCharging: numericalBoolean == 1,
-      celsius: Double(rawTemperature) - 81
+    // Literal on-screen number (interpret using `isFahrenheit`)
+    let temperatureCurrent = data.bytes[4]
+
+    // Literal on-screen number (interpret using `isFahrenheit`)
+    let temperatureTarget = data.bytes[5]
+
+    let isCharging = data.bytes[6] == 1
+    guard [0, 1].contains(data.bytes[6]) else {
+      throw BluetoothParsingError.unexpectedValue("\(Self.self) isCharging \(isCharging)")
+    }
+
+    let batteryBars = data.bytes[7]
+    guard (0...5).contains(batteryBars) else {
+      throw BluetoothParsingError.unexpectedValue("\(Self.self) batteryBars \(batteryBars)")
+    }
+
+    self.value = Contents(
+      battery: BatteryState(
+        percent: Double(batteryBars) / 5,
+        isCharging: isCharging
+      ),
+      isHeating: isHeating,
+      tempCurrent: TemperatureState(
+        celsius: Measurement<UnitTemperature>.init(
+          value: Double(temperatureCurrent),
+          unit: unit
+        )
+        .converted(to: .celsius)
+        .value,
+        unit: unit
+      ),
+      tempTarget: .celsius(
+        Measurement<UnitTemperature>.init(
+          value: Double(temperatureTarget),
+          unit: unit
+        )
+        .converted(to: .celsius)
+        .value
+      )
     )
   }
 }
--- MuggleCore/Sources/VFZOBluetooth/VFZOMug+BluetoothMug.swift
+++ MuggleCore/Sources/VFZOBluetooth/VFZOMug+BluetoothMug.swift
@@ -12,19 +12,19 @@ extension VFZOMug: BluetoothMug {
 extension VFZOMug {
 
   var activity: MugActivity? {
-    fatalError()
+    push.value?.activity
   }
 
   var activityStream: AnyPublisher<MugActivity, Never> {
-    fatalError()
+    push.$value.compactMap(\.?.activity).eraseToAnyPublisher()
   }
 
   var batteryState: BatteryState? {
-    fatalError()
+    push.value?.battery
   }
 
   var batteryStream: AnyPublisher<BatteryState, Never> {
-    fatalError()
+    push.$value.compactMap(\.?.battery).eraseToAnyPublisher()
   }
 
   var connectionStream: AnyPublisher<ConnectionStatus, Never> {
@@ -32,11 +32,11 @@ extension VFZOMug {
   }
 
   var hasContents: Bool? {
-    fatalError()
+    nil
   }
 
   var hasContentsStream: AnyPublisher<Bool, Never> {
-    fatalError()
+    Empty(outputType: Bool.self, failureType: Never.self).eraseToAnyPublisher()
   }
 
   var isConfiguringStream: AnyPublisher<Bool, Never> {
@@ -68,11 +68,11 @@ extension VFZOMug {
   }
 
   var led: LEDState? {
-    fatalError()
+    nil
   }
 
   var ledStream: AnyPublisher<LEDState, Never> {
-    fatalError()
+    Empty(outputType: LEDState.self, failureType: Never.self).eraseToAnyPublisher()
   }
 
   var serialNumber: String? {
@@ -80,30 +80,54 @@ extension VFZOMug {
   }
 
   var temperatureStream: AnyPublisher<AllTemperatureState, Never> {
-    fatalError()
+    push.$value
+      .compactMap { $0 }
+      .map { state in
+        AllTemperatureState(
+          currentCelsius: state.tempCurrent.celsius,
+          target: state.tempTarget,
+          unit: state.tempCurrent.unit
+        )
+      }
+      .eraseToAnyPublisher()
   }
 
   var temperatureCurrentStream: AnyPublisher<TemperatureState, Never> {
-    fatalError()
+    push.$value.compactMap(\.?.tempCurrent).eraseToAnyPublisher()
   }
 
   var temperatureTargetStream: AnyPublisher<LocalUnit<HeaterState>, Never> {
-    fatalError()
+    push.$value.compactMap(\.?.localHeaterState).eraseToAnyPublisher()
   }
 
   var temperatureUnitStream: AnyPublisher<UnitTemperature, Never> {
-    fatalError()
+    push.$value.compactMap(\.?.tempCurrent.unit).eraseToAnyPublisher()
   }
 
   var temperatureViableRange: ClosedRange<Double> {
-    fatalError()
+    PushEventCharacteristic.validTargetTemperatureRange
   }
 
   var temperatureCurrent: TemperatureState? {
-    fatalError()
+    push.value?.tempCurrent
   }
 
   var temperatureTarget: LocalUnit<HeaterState>? {
-    fatalError()
+    push.value?.localHeaterState
+  }
+}
+
+private extension PushEventCharacteristic.Contents {
+  var activity: MugActivity {
+    guard isHeating else { return .standby }
+    guard case .celsius(let target) = tempTarget else { return .standby }
+    let targetRange = (target - 0.5)...(target + 0.5)
+    if targetRange.contains(tempCurrent.celsius) { return .holding }
+    if tempCurrent.celsius < target { return .heating }
+    return .cooling
+  }
+
+  var localHeaterState: LocalUnit<HeaterState> {
+    .init(temp: tempTarget, unit: tempCurrent.unit)
   }
 }
